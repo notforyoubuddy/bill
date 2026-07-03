c10cdc4cd7cb83470e3c9305a56e2b2448602a61
notforyoubuddy <newdistractionforu@gmail.com>
2026-07-03 20:54:33 +0800

    Add files via upload

---
+++ password.js
@@ -0,0 +1,17 @@
+var password = "marioluigi";
+function passcheck() {
+if(document.getElementById('pass1').value != password) {
+alert("Incorrect password ba is liw");
+return false;
+}
+}
+
+if(document.getElementById('pass1').value != password) {
+alert('Correct password, ano tara?');
+
+}
+
+{
+  "source"; "/(.*)",
+  "destination"; "/index.html"
+}
